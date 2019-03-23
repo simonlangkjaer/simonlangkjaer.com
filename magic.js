c9bca8fdb0cbe2a6e7fe4d23f17bf46e353eddda
simonlangkjaer <langkjrn@gmail.com>
2019-03-23 10:34:44 +0100

    Commit Simonlangkjaer.com

---
+++ magic.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel(
+    {
+      nav: true,
+      items: 2,
+      loop: true
+    }
+  );
+});
---
+++ tlfp.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+      {
+        nav: true,
+        items: 2,
+        loop: true
+      }
+    );
+  });
+  
